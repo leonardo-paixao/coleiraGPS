12f048cd6d4f1857ce8fdfa43c3954051662d589
Leonardo Paixão <leonardomiranda.paixao@hotmail.com>
2023-02-09 09:35:02 -0300

    Aplicativo Flutter
    Adicionando o aplicativo que irá receber as informações do GPS.

---
+++ front_end/pet_tracker_app/pet_tracker/lib/pages/myapp.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
